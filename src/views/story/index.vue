12da1e480ac3ae1f62625b8748beea136b1912ee
twinkleding <twinkle415@163.com>
2025-09-10 10:45:34 +0800

    修改

--- src/views/story/index.vue
+++ src/views/story/index.vue
@@ -10,21 +10,26 @@
   <div class="story">
     <back />
     <el-scrollbar>
-      <div class="text" v-html="text"></div>
+      <div class="text">
+        <p v-for="item in text.split('\n')">
+        <h3 v-if="item.includes('###')">{{ item.replace('###', '') }}</h3>
+        <template v-else>{{ item }}</template>
+        </p>
+      </div>
     </el-scrollbar>
   </div>
 </template>
 <script setup>
 import { ref, onMounted } from "vue";
 import axios from "axios";
-import markdown from "markdown";
 import Back from "@/components/Back.vue";
 
 const text = ref("");
 
 const getStory = async () => {
-  const res = await axios.get("./story/小说.md");
-  text.value = markdown.markdown.toHTML(res.data);
+  const res = await axios.get("./story/小说.txt");
+
+  text.value = res.data;
 };
 
 onMounted(() => {
@@ -37,11 +42,18 @@ onMounted(() => {
   height: 100%;
   width: 100%;
   overflow: hidden;
+
   .text {
     margin: 0 auto;
     width: 800px;
     height: 100%;
     overflow-y: auto;
+    padding-bottom: 100px;
+
+    p {
+      text-indent: 2em;
+      line-height: 32px;
+    }
   }
 }
 </style>
